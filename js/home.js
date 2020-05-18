a3fe9c61b41aaab54c6b99f8fbe1af2b950d3e45
Jénaïc Cambré <me@jenaiccambre.com>
2020-05-18 15:38:39 +0200

    day5

--- js/home.js
+++ js/home.js
@@ -2,6 +2,7 @@ let curSection = 'development';
 const initHome = () => {
     const bodyEl = document.querySelector('body.page-home');
     const hexaOuterEl = document.querySelector('.outer-hexagon');
+    const asideEl = document.querySelector('aside');
     bodyEl.addEventListener('mouseover', ({ target }) => {
         if (target.matches('section') === true) {
             if (target.classList[0] !== curSection) {
@@ -20,10 +21,58 @@ const initHome = () => {
     });
 
     bodyEl.addEventListener('click', ({ target }) => {
-            bodyEl.classList.add('page-transition');
-            bodyEl.classList.add('page-about-transition');
+        console.log('target', target);
+        if (target.matches('section') === true) {
+            // console.log('target2', [...target.classList].includes('about'));
+            const category = target.getAttribute('data-category');
+            const positions = getCategoryPositions({ el: target });
+            setHexagonPos({ el: asideEl, category });
+
+            console.log('positions', positions);
+
+            setTimeout(() => {
+                bodyEl.classList.add('page-transition');
+                bodyEl.classList.add(`page-${category}-transition`);
+            }, 0);
             // alert('ok');
+        }
     });
-}
+};
+
+const setHexagonPos = ({ el, category }) => {
+    let xProp = 'left';
+    let yProp = 'top';
+    if (['development', 'writing', 'teaching'].includes(category)) {
+        xProp = 'right';
+    }
+    if (['development', 'photography'].includes(category)) {
+        yProp = 'bottom';
+    }
+    const xPos = getComputedStyle(el).getPropertyValue(xProp);
+    const yPos = getComputedStyle(el).getPropertyValue(yProp);
+    el.setAttribute('style', `${xProp}: ${xPos}; ${yProp}: ${yPos};`);
+};
+
+const getCategoryPositions = ({ el }) => {
+    const styles = getComputedStyle(el);
+    let top = styles.getPropertyValue('top');
+    let left = styles.getPropertyValue('left');
+    const width = styles.getPropertyValue('width');
+    const height = styles.getPropertyValue('height');
+
+    if (top === 'auto') {
+        top = `${el.offsetTop}px`;
+    }
+    if (left === 'auto') {
+        left = `${el.offsetLeft}px`;
+    }
+
+    return {
+        top,
+        left,
+        width,
+        height
+    };
+};
 
 window.onload = initHome;
